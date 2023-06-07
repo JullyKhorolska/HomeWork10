133ba2a5a04251ab60d8934660976b1c7368fc0f
Jully Khorolska <uxa0608@gmail.com>
2023-06-08 00:22:46 +0300

    duplicates

--- src/dublicates/ChooseDuplicates.java
+++ src/dublicates/ChooseDuplicates.java
@@ -2,20 +2,36 @@ package dublicates;
 
 import java.util.*;
 
-
-public class ChooseDublicates {
+public class ChooseDuplicates {
     public static void main(String[] args) {
-        List listWithDublicates = new ArrayList(20);
-        for (int i = 1; i < 10; i++) {
-            listWithDublicates.add (i, i);
+        List<Integer> listWithDuplicates = new ArrayList <>();
+        List<Integer> listWithOutDuplicates = new ArrayList <>();
+        for (int i = 0; i < 100; i++) {
+            listWithDuplicates.add(i, (int) (Math.random() * 50 - 1));
         }
-//        listWithDublicates.add(1);
-//        listWithDublicates.add(2);
-//        listWithDublicates.add(3);
+        listWithOutDuplicates.add(listWithDuplicates.get(0));
+        int count = 0;
 
-        for (Object dublicates : listWithDublicates) {
-            System.out.println(dublicates);
+        for (int i = 0; i < listWithDuplicates.size() - 1; i++) {
+            if (!listWithOutDuplicates.contains(listWithDuplicates.get(i))) {
+                listWithOutDuplicates.add(listWithDuplicates.get(i));
+            } else count++;
         }
+        Collections.sort(listWithDuplicates);
+        Collections.sort(listWithOutDuplicates);
 
+        System.out.println("-----------Collection with duplicates--------------");
+
+        System.out.println(Arrays.toString(listWithDuplicates.toArray()));
+        System.out.println("Size of collection: " + listWithDuplicates.size());
+
+        System.out.println("\n----------Collection without duplicates-------------");
+
+        System.out.println(Arrays.toString(listWithOutDuplicates.toArray()));
+        System.out.println("Size of collection: " + listWithOutDuplicates.size());
+
+        System.out.println("------------------------------------");
+
+        System.out.println("count of duplicates: " + count);
     }
 }
