251db2939d82538ebdfdd9454a52ba63764a3b8b
Jully Khorolska <uxa0608@gmail.com>
2023-06-11 21:24:28 +0300

    Check people in the circle

--- src/peopleInTheCircle/CheckPeople.java
+++ src/peopleInTheCircle/CheckPeople.java
@@ -3,24 +3,24 @@ package peopleInTheCircle;
 import java.util.*;
 
 public class CheckPeople {
-    public static void main(String[] args){
-        int N = 99;
+    public static void main(String[] args) {
+        int N = 11;
         PeopleInTheCircle people = new PeopleInTheCircle();
         List<Integer> arrayList = new ArrayList<>();
         LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            arrayList.add(i + 1);
-        }
-        linkedList.addAll(arrayList);
-        System.out.println(Arrays.toString(arrayList.toArray()));
+        if (N > 0) {
+            for (int i = 0; i < N; i++) {
+                arrayList.add(i + 1);
+            }
+            linkedList.addAll(arrayList);
+            System.out.println(Arrays.toString(arrayList.toArray()));
+            long startTime = System.currentTimeMillis();
+            people.removeEverySecondPeople(arrayList);
+            System.out.println("Execution time in milliseconds time for ArrayList: " + ((double) System.currentTimeMillis() - startTime) + "\n");
 
-        long startTime = System.currentTimeMillis();
-        people.removeEverySecondPeople(arrayList);
-        System.out.println("Execution time in milliseconds time for ArrayList: " + ((double)System.currentTimeMillis() - startTime) + "\n");
-
-        startTime = System.currentTimeMillis();
-        people.removeEverySecondPeople(linkedList);
-        System.out.println("Execution time in milliseconds time for LinkedList: " + ((double)System.currentTimeMillis() - startTime));
-
+            startTime = System.currentTimeMillis();
+            people.removeEverySecondPeople(linkedList);
+            System.out.println("Execution time in milliseconds time for LinkedList: " + ((double) System.currentTimeMillis() - startTime));
+        } else System.out.println("There are no any people in the circle");
     }
 }
--- src/peopleInTheCircle/PeopleInTheCircle.java
+++ src/peopleInTheCircle/PeopleInTheCircle.java
@@ -6,14 +6,12 @@ public class PeopleInTheCircle {
     public PeopleInTheCircle() {
 
     }
-
     public void removeEverySecondPeople(List<Integer> numList) {
         boolean flag = false;
         while (numList.size() != 1) {
             for (int i = 0; i < numList.size(); i++) {
                 if (flag) {
-//                   numList.remove(i--);
-                   System.out.println("remove: " + numList.remove(i--));
+                   numList.remove(i--);
                 }
                 flag = !flag;
             }
